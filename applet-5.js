2931d4532fe88cab9ffd16635cf235f73c998a35
clintambacan <clintambacan@gmail.com>
2024-10-30 21:00:26 +0800

    Complete fetchWeather method

--- applet-5.js
+++ applet-5.js
@@ -44,3 +44,16 @@ class WeatherApp{
             alert('Please enter your API key.');
             return;
         }
+        const city = this.cityInput.value;
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+        
+    }
