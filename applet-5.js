c1955608205050d93542298195b884408fa2f37f
clintambacan <clintambacan@gmail.com>
2024-10-30 20:58:50 +0800

    Add WeatherService class

--- applet-5.js
+++ applet-5.js
@@ -28,4 +28,12 @@ class WeatherApp{
         this.windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
         
         this.weatherCard.style.display = 'block';
+
+    }
+
+    class WeatherService extends WeatherApp {
+        constructor() {
+            super();
+            this.apiKey = '';
+        }
     }
